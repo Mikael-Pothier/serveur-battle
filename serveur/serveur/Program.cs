f1420a7e9e26bb78b608c6444d32ad3fad646c6c
qcfrodon <mariobros_ch@hotmail.fr>
2014-09-22 14:37:12 -0400

    debut

---
+++ serveur/serveur/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace serveur
+{
+    class Program
+    {
+        static byte [] buffer {get; set;}
+        static Socket sck;
+
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0,1234));
+            sck.Listen(1000);
+            Socket client1 =sck.Accept();
+            Socket client2 = sck.Accept();
+        }
+    }
+}
