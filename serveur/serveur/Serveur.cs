7f6491ebbd7a1e91c3bac7143301c2f6a3ab1483
qcfrodon <mariobros_ch@hotmail.fr>
2014-09-30 15:01:16 -0400

    here we go again

--- serveur/serveur/Serveur.cs
+++ serveur/serveur/Serveur.cs
@@ -55,25 +55,106 @@ namespace serveur
                     player2=ReceiveDataBateau(client2);
                     setMatrice(player1, matriceAttaqueJ1);
                     setMatrice(player2, matriceAttaqueJ2);
-
-                    position PositionAttaque = new position();
-                    do
-                    {
-                        client2.Blocking = true;
-                        ReceiveData(client1,PositionAttaque);
-                        if (estToucher(PositionAttaque,matriceAttaqueJ2))
-                        {
-                            PositionAttaque = null;
-                            //envoyer message client
-                        }
-                    } while (PositionAttaque == null) ;
-                    int resultat = getResultat(PositionAttaque, matriceAttaqueJ2);
-                    setResultat(PositionAttaque,matriceAttaqueJ2,resultat);
+                    attaquer(client1, client2, player2, matriceAttaqueJ2);
                 }
             }
             client1.Close();
             client2.Close();
             sck.Close();
+        }
+        private static void attaquer(Socket clientAttaque, Socket clientWait,Joueur playerAttaquer, int[,] matrice)
+        {
+            position PositionAttaque = new position();
+            do
+            {
+                clientWait.Blocking = true;
+                ReceiveData(clientAttaque, PositionAttaque);
+                if (estToucher(PositionAttaque, matrice))
+                {
+                    PositionAttaque = null;
+                    sendClient(clientAttaque,"attaquer une place valide");
+                }
+            } while (PositionAttaque == null);
+            int resultat = getResultat(PositionAttaque, matrice);
+            setResultat(PositionAttaque, matrice, resultat);
+            String message = "";
+            if (resultat == touche)
+            {
+                message = BateauTouche(PositionAttaque, playerAttaquer);
+            }
+            else
+            {
+                message = "vous avez manquer la cible";
+            }
+            String text = resultat + "," + PositionAttaque.x.ToString() + "," + PositionAttaque.y.ToString() + "," + message;
+            sendClient(clientAttaque, text);
+            sendClient(clientWait, text);            
+        }
+        private static String BateauTouche(position pos,Joueur player)
+        {
+            bool trouvé = false;
+            String message = "vous avez touche(e) un bateau";
+            for (int i = 0;!porteAvionCoule(player) && i < player.PorteAvion.longueur && !trouvé; ++i)
+            {
+                if (player.PorteAvion.cases[i].x == pos.x && player.PorteAvion.cases[i].y == pos.y)
+                {
+                    player.PorteAvion.estTouche[i] = true;
+                    if (porteAvionCoule(player))
+                        message="Porte avion coule!";
+                    trouvé = true;
+                }
+            }
+
+            for (int i = 0; !ContreTorpilleCoule(player) && i < player.ContreTorpille.longueur && !trouvé; ++i)
+            {
+                if (player.ContreTorpille.cases[i].x == pos.x && player.ContreTorpille.cases[i].y == pos.y)
+                {
+                    player.ContreTorpille.estTouche[i] = true;
+                    if(ContreTorpilleCoule(player))
+                        message="ContreTorpille coule";
+                    trouvé = true;
+                }
+            }
+            for (int i = 0; !CroiseurCoule(player) && i < player.Croiseur.longueur && !trouvé; ++i)
+            {
+                if (player.Croiseur.cases[i].x == pos.x && player.Croiseur.cases[i].y == pos.y)
+                {
+                    player.Croiseur.estTouche[i] = true;
+                    if(CroiseurCoule(player))
+                        message= "Croiseur coule";
+                    trouvé = true;
+                }
+            }
+            for (int i = 0; !SousMarinCoule(player) && i < player.SousMarin.longueur && !trouvé; ++i)
+            {
+                if (player.SousMarin.cases[i].x == pos.x && player.SousMarin.cases[i].y == pos.y)
+                {
+                    player.SousMarin.estTouche[i] = true;
+                    if (SousMarinCoule(player))
+                        message = "Sous-marin coule";
+                    trouvé = true;
+                }
+            }
+            for (int i = 0; !TorpilleurCoule(player) && i < player.Torpilleur.longueur && !trouvé; ++i)
+            {
+                if (player.Torpilleur.cases[i].x == pos.x && player.Torpilleur.cases[i].y == pos.y)
+                {
+                    player.Torpilleur.estTouche[i] = true;
+                    if (TorpilleurCoule(player))
+                        message = "Torpilleur coule";
+                    trouvé = true;
+                }
+            }
+            return message;
+        }
+        private static void sendClient(Socket client,String text) 
+        {
+            try
+            {
+                byte[] data = Encoding.ASCII.GetBytes(text);
+                client.Send(data);
+            }
+            catch { Console.Write("Erreur de telechargement des donnees"); }
         }
         private static void setResultat(position pos, int[,] matrice,int resultat)
         {
@@ -91,7 +172,7 @@ namespace serveur
         }
         private static bool estToucher(position pos,int [,] matrice)
         {
-                if (matriceAttaqueJ2[pos.x, pos.y] == touche || matriceAttaqueJ2[pos.x, pos.y] == manque) 
+                if (matriceAttaqueJ2[pos.x, pos.y] != touche && matriceAttaqueJ2[pos.x, pos.y] != manque) 
                 {
                     return false;
                 }
@@ -206,6 +287,51 @@ namespace serveur
             {
 
             }
+        }
+        private static bool porteAvionCoule(Joueur player)
+        {
+            for (int i = 0; i < player.PorteAvion.longueur; ++i)
+            {
+                if (player.PorteAvion.estTouche[i] == false)
+                    return false;
+            }
+            return true;
+        }
+        private static bool ContreTorpilleCoule(Joueur player)
+        {
+            for (int i = 0; i < player.ContreTorpille.longueur; ++i)
+            {
+                if (player.ContreTorpille.estTouche[i] == false)
+                    return false;
+            }
+            return true;            
+        }
+        private static bool SousMarinCoule(Joueur player)
+        {
+            for (int i = 0; i < player.SousMarin.longueur; ++i)
+            {
+                if (player.SousMarin.estTouche[i] == false)
+                    return false;
+            }
+            return true;
+        }
+        private static bool CroiseurCoule(Joueur player)
+        {
+            for (int i = 0; i < player.Croiseur.longueur; ++i)
+            {
+                if (player.Croiseur.estTouche[i] == false)
+                    return false;
+            }
+            return true;
+        }
+        private static bool TorpilleurCoule(Joueur player)
+        {
+            for (int i = 0; i < player.Torpilleur.longueur; ++i)
+            {
+                if (player.Torpilleur.estTouche[i] == false)
+                    return false;
+            }
+            return true;
         }
     }
 }
